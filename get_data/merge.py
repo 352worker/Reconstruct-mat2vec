8893a9791b90be8c30f8564637768fbecf9de27e
352worker <m202170925@hust.edu.cn>
2022-11-28 10:21:16 +0800

    1

--- get_data/merge.py
+++
@@ -1,7 +0,0 @@
-from tqdm import tqdm
-for year in range(2001,2022):
-    f0 = open("./final/%d.txt" %year, 'w', encoding="utf-8")
-    for i in tqdm(range(1921, year+1)):
-        f = open("./text-v0/%dpara.txt" %i, 'r', encoding="utf-8")
-        for line in f:
-            f0.write(line)
